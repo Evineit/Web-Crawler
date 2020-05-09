76b8efd0fa84af1703ec8d3ea1fce1795870a7f8
Kevin R <seidaikevin@gmail.com>
2020-05-09 17:52:15 -0500

    to table

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,12 +1,16 @@
 package crawler;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,6 +25,7 @@ public class WebCrawler extends JFrame {
     JTextField textFieldURL;
     JButton buttonDownload;
     JLabel labelTitle;
+    JTable table = new JTable(new DefaultTableModel(new String[0][0], new String[]{"Titles", "Links"}));
     public WebCrawler() {
         super("Web crawler");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,37 +35,45 @@ public class WebCrawler extends JFrame {
         buttonDownload = new JButton("Get text!");
         textFieldURL = new JTextField();
         labelTitle = new JLabel("Title:");
+//        table.addColumn(new TableColumn());
+//        table.
+        JScrollPane scrollPane =  new JScrollPane(table);
         topPanel.setLayout(new BoxLayout(topPanel,BoxLayout.Y_AXIS));
         mainPanel.setLayout(new BorderLayout());
         iTopPanel.setLayout(new BoxLayout(iTopPanel,BoxLayout.X_AXIS));
         urlPanel.setLayout(new BoxLayout(urlPanel,BoxLayout.X_AXIS));
 
         labelTitle.setName("TitleLabel");
-        textArea.setName("HtmlTextArea");
+//        textArea.setName("HtmlTextArea");
+        table.setName("TitlesTable");
         buttonDownload.setName("RunButton");
         textFieldURL.setName("UrlTextField");
         textArea.setText("HTML code?");
-        textArea.setEnabled(false);
+        table.setEnabled(false);
         topPanel.add(urlPanel);
         topPanel.add(iTopPanel);
         urlPanel.add(textFieldURL);
         urlPanel.add(buttonDownload);
         iTopPanel.add(labelTitle);
         add(topPanel,BorderLayout.NORTH);
-        add(textArea,BorderLayout.CENTER);
+        add(scrollPane,BorderLayout.CENTER);
         buttonDownload.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                textArea.setText(downloadSource());
+//                textArea.setText(downloadSource());
+                downloadSource();
+                revalidate();
+                repaint();
             }
         });
         setVisible(true);
 
     }
-    String downloadSource(){
+    void downloadSource(){
         final String url = textFieldURL.getText();
-
+//        Map<String, String> hashMap = new HashMap<>();
         final InputStream inputStream;
+        String links[][];
         try {
             inputStream = new URL(url).openStream();
             final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
@@ -72,14 +85,25 @@ public class WebCrawler extends JFrame {
                 stringBuilder.append(LINE_SEPARATOR);
             }
             Pattern pattern= Pattern.compile("<title>(.*)</title>");
-            Matcher matcher = pattern.matcher(stringBuilder.toString());
-            matcher.find();
-            labelTitle.setText(matcher.group(1));
-            return  stringBuilder.toString();
+            Matcher matcherTitle = pattern.matcher(stringBuilder.toString());
+            matcherTitle.find();
+            labelTitle.setText(matcherTitle.group(1));
+//            Pattern patternLink = Pattern.compile("<a id=\".*\".*href=\"(.*)\".{0,100}title=\"(.*?)\" .*>");
+            Pattern patternLink = Pattern.compile("<a target=\"(.*)\" href=\"(.*?)\">");
+            Matcher matcherLinks = patternLink.matcher(stringBuilder.toString());
+            int size = (int) matcherLinks.results().count();
+            links= new String[size][2];
+            int i = 0;
+            matcherLinks.reset();
+            while (matcherLinks.find()){
+                links[i][0] = matcherLinks.group(1);
+                links[i][1] = matcherLinks.group(2);
+                i++;
+            }
+            table.setModel(new DefaultTableModel(links, new String[]{"URL", "Title"}));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
 
 
     }
